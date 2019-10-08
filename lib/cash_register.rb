e82c545d658548669365d4d9a7ad1d1f1f84f46e
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-08 13:36:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,16 @@
 class CashRegister
+  
   def cash_register(new)
     @cash_register = new
   end
+  
   def @cash_register
     @cash_register = new
   end
+  
   def @cash_register_with_discount
     @cash_register_with_discount = new(20)
   end
+  
   end
 end
