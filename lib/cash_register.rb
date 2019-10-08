89c1e0080833bfd228990d7ad5fed8be41a072d0
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-08 14:05:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,13 @@
 require 'pry'
 class CashRegister
  attr_accessor :items, :discount, :total, :last_transaction
-  ## self.total = 0
-    self.discount = discount
-    self.items = []
-    self.last_transaction_amount = 0
-  end
-
+  #def initialize(discount=0)
+   # self.total = 0
+    #self.discount = discount
+    #self.items = []
+    #self.last_transaction_amount = 0
+  #end
+  
   def total=(value)
     @total = value
   end
